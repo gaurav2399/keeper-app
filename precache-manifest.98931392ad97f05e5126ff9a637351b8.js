ba143110663b20648fedf7769355dc8e45dd9f2a
Gaurav <gs926189@gmail.com>
2022-01-15 03:54:32 +0530

    Updates

---
+++ precache-manifest.98931392ad97f05e5126ff9a637351b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c2952b384c7bdbceb2164a4448b0a0",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "8825290846a9a17a455e",
+    "url": "/keeper-app/static/js/2.e3bafcba.chunk.js"
+  },
+  {
+    "revision": "2cb3094510b7e98760c0",
+    "url": "/keeper-app/static/js/main.81c380d6.chunk.js"
+  },
+  {
+    "revision": "e4ad95b7ee73d89007ed",
+    "url": "/keeper-app/static/js/runtime-main.fc0473d0.js"
+  }
+]);
